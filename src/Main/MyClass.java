3c480a036d3517e189ede4ea81443e9401dbe311
Vandal <Leonard-2@yandex.ru>
2018-03-01 23:31:59 +0300

    Initial commit new test2

---
+++ src/Main/MyClass.java
@@ -0,0 +1,16 @@
+package Main;
+
+public class MyClass {
+    int a, b;
+
+    public MyClass (short a, short b) {
+        this.a = a;
+        this.b = b;
+    }
+    public static void Sum(int a, int b) {
+        int i = a + b;
+        System.out.println(i);
+    }
+
+
+}
---
+++ src/Main/index.java
@@ -0,0 +1,7 @@
+import Main.MyClass;
+
+class Index {
+    public static void main(String[] args) {
+        MyClass.Sum(2,2);
+    }
+}

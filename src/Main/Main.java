192d79690daa52c6045556c5fafb8c61bac1b9a7
Vandal <Leonard-2@yandex.ru>
2018-03-02 00:08:34 +0300

    Initial commit new test2 test3

---
+++ src/Main/Main.java
@@ -0,0 +1,10 @@
+package Main;
+import Main.MyClass;
+
+public class Main {
+    public static void main(String[] args) {
+        MyClass.Sum(2, 2);
+        System.out.println("line");
+
+    }
+}
--- src/Main/index.java
+++
@@ -1,7 +0,0 @@
-import Main.MyClass;
-
-class Index {
-    public static void main(String[] args) {
-        MyClass.Sum(2,2);
-    }
-}
